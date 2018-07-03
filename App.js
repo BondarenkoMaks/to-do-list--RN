e32af64c01956909324614a591e877aec0a90573
maksym.bondarenko <maksym.bondarenko@strikersoft.com>
2018-07-03 11:12:45 +0300

    added app

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Main from './src/index';
+export default class App extends React.Component {
+  render() {
+    return <Main />;
+  }
+}
